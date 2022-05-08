fce9920bd28af919ac6d155dba26293f964eeb07
bin.zhan <bin.zhan@moying.ai>
2022-05-08 17:52:58 +0800

    feat: added README and Vert.x Framework

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  kotlin ("jvm") version "1.6.10"
+  application
+  id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+group = "ai.moying"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+val vertxVersion = "4.2.7"
+val junitJupiterVersion = "5.8.2"
+
+val mainVerticleName = "io.moying.interview.homework.MainVerticle"
+val launcherClassName = "io.vertx.core.Launcher"
+
+val watchForChange = "src/**/*"
+val doOnChange = "${projectDir}/gradlew classes"
+
+application {
+  mainClass.set(launcherClassName)
+}
+
+dependencies {
+  implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+  implementation("io.vertx:vertx-config")
+  implementation("io.vertx:vertx-web")
+  implementation("io.vertx:vertx-lang-kotlin")
+
+  implementation("ch.qos.logback:logback-classic:1.2.10")
+  implementation("org.slf4j:slf4j-api:1.7.36")
+
+  implementation(kotlin("stdlib-jdk8"))
+  testImplementation("io.vertx:vertx-junit5")
+  testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "11"
+
+tasks.withType<ShadowJar> {
+  archiveClassifier.set("fat")
+  manifest {
+    attributes(mapOf("Main-Verticle" to mainVerticleName))
+  }
+  mergeServiceFiles()
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events = setOf(PASSED, SKIPPED, FAILED)
+  }
+}
+
+tasks.withType<JavaExec> {
+  args = listOf("run", mainVerticleName, "--redeploy=$watchForChange", "--launcher-class=$launcherClassName", "--on-redeploy=$doOnChange")
+}
